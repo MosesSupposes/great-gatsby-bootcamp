1b1d65385820b31a85548ee103dbaaf603b8e28b
Moses A. Samuel <mosamuel98@gmail.com>
2020-08-01 15:36:14 -0700

    add styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,54 @@
 import React from "react"
 import { Link } from "gatsby"
 
+// import "./header.module.scss"
+import headerStyles from "./header.module.scss"
+
 export default function Header() {
   return (
-    <header>
-      <h1>Moses A. Samuel</h1>
+    <header className={headerStyles.header}>
+      <h1>
+        <Link to="/" className={headerStyles.title}>
+          Moses A. Samuel
+        </Link>
+      </h1>
       <nav>
-        <ul>
+        <ul className={headerStyles.navList}>
           <li>
-            <Link to="/">Home</Link>
+            <Link
+              to="/"
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+            >
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/blog">Blog</Link>
+            <Link
+              to="/blog"
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+            >
+              Blog
+            </Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link
+              to="/about"
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+            >
+              About
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link
+              to="/contact"
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+            >
+              Contact
+            </Link>
           </li>
         </ul>
       </nav>
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,18 @@
 import React from "react"
 import Header from "./header"
 import Footer from "./footer"
+
 import "../styles/index.scss"
+import layoutStyles from "./layout.module.scss"
 
 export default function Layout({ children }) {
   return (
-    <>
-      <Header />
-      {children}
+    <div className={layoutStyles.container}>
+      <div className={layoutStyles.content}>
+        <Header />
+        {children}
+      </div>
       <Footer />
-    </>
+    </div>
   )
 }
