8b281aa7df3bd6f9b8de518a371abe45dc26bace
Moses A. Samuel <mosamuel98@gmail.com>
2020-08-02 14:36:15 -0700

    add the ability to source content from the filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,5 +9,14 @@ module.exports = {
     title: "Full-Stack Bootcamp",
     author: "Moses A. Samuel",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+  ],
 }
