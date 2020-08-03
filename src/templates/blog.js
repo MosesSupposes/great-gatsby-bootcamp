760825ab480fa7b23487aacf2115da797ffb13b4
Moses A. Samuel <mosamuel98@gmail.com>
2020-08-02 18:50:48 -0700

    dynamically add post data into each blog post

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,6 +1,29 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
 
-export default function Blog() {
-  return <Layout>This is the blog template</Layout>
+// Beacause we exported a graphql query before rendering our JSX, the resolved value of this query gets passed as props to our component.
+export const query = graphql`
+  query blogPost($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+
+// Don't forget that unlike with `useStaticQuery`, the graphql data gets stored under an attribute called `data`.
+export default function Blog(props) {
+  return (
+    <Layout>
+      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+      <p>{props.data.markdownRemark.frontmatter.date}</p>
+      <div
+        dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
+      ></div>
+    </Layout>
+  )
 }
