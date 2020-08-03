3003a4cdc4ade7e3f71c576d94d55959e066808a
Moses A. Samuel <mosamuel98@gmail.com>
2020-08-02 17:15:07 -0700

    render post previews for each blog post on the "blog" page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,37 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 import Layout from "../components/layout"
 
 export default function BlogPage() {
+  const data = useStaticQuery(graphql`
+    query allBlogPosts {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  console.log(data)
   return (
     <Layout>
       <h1>My Blog</h1>
-      <p>Posts will show up here later on</p>
+      <ol>
+        {data.allMarkdownRemark.edges.map(function renderPreview(edge, index) {
+          return (
+            <li key={index}>
+              <h2>{edge.node.frontmatter.title}</h2>
+              <p>{edge.node.frontmatter.date}</p>
+            </li>
+          )
+        })}
+      </ol>
     </Layout>
   )
 }
