6dea28cb7eebbbfa02f1ff0838cfd2b967334b3f
Moses A. Samuel <mosamuel98@gmail.com>
2020-08-02 17:41:18 -0700

    generate slugs dynamically for each blog post

---
+++ gatsby-node.js
@@ -0,0 +1,14 @@
+const path = require("path")
+
+exports.onCreateNode = function generateAndAttachSlug({ node, actions }) {
+  const { createNodeField } = actions
+
+  if (node.internal.type === "MarkdownRemark") {
+    const slug = path.basename(node.fileAbsolutePath, ".md")
+    createNodeField({
+      node,
+      name: "slug",
+      value: slug,
+    })
+  }
+}
