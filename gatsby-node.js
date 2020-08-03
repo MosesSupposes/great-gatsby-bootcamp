ac988ff963b654e42f920fb6b58e45e49ba269fd
Moses A. Samuel <mosamuel98@gmail.com>
2020-08-02 18:21:02 -0700

    create a page for each of the blog posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,3 +12,34 @@ exports.onCreateNode = function generateAndAttachSlug({ node, actions }) {
     })
   }
 }
+
+exports.createPages = async function createPagesForBlogPosts({
+  graphql,
+  actions,
+}) {
+  const { createPage } = actions
+  // Fetch all slugs
+  const { data } = await graphql(`
+    query allSlugs {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  // Create an individual page for each slug, passing in the slug as context
+  data.allMarkdownRemark.edges.forEach(edge => {
+    createPage({
+      component: path.resolve("./src/templates/blog.js"),
+      path: `/blog/${edge.node.fields.slug}`,
+      context: {
+        slug: edge.node.fields.slug,
+      },
+    })
+  })
+}
---
+++ src/templates/blog.js
@@ -0,0 +1,6 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function Blog() {
+  return <Layout>This is the blog template</Layout>
+}
