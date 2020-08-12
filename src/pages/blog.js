2cbf5e6f9bddd407ae494da8d7cd8f1e60fcb9ba
Moses A. Samuel <mosamuel98@gmail.com>
2020-08-12 16:20:17 -0700

    add styles to the blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql, useStaticQuery, Link } from "gatsby"
 import Layout from "../components/layout"
+import blogStyles from "./blog.module.scss"
 
 export default function BlogPage() {
   const data = useStaticQuery(graphql`
@@ -25,10 +26,10 @@ export default function BlogPage() {
   return (
     <Layout>
       <h1>My Blog</h1>
-      <ol>
+      <ol className={blogStyles.posts}>
         {data.allMarkdownRemark.edges.map(function renderPreview(edge, index) {
           return (
-            <li key={index}>
+            <li className={blogStyles.post} key={index}>
               <Link to={`/blog/${edge.node.fields.slug}`}>
                 <h2>{edge.node.frontmatter.title}</h2>
                 <p>{edge.node.frontmatter.date}</p>
